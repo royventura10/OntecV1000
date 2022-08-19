2f405b44983ab1896a1c2d39b4e638466ae62634
RoyVentura <73194744+royventura10@users.noreply.github.com>
2022-08-19 00:50:16 -0500

    Se agrega proyecto base

---
+++ src/test/java/net/ontec/ontecv1/OntecV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ontec.ontecv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OntecV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
